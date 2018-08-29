9282d94dc5b775ba84cc7d2131dfdab5c4aa793a
Luo <zxhy617195600@163.com>
2018-08-29 12:37:26 +0800

    Update img_addnum.py

--- img_addnum.py
+++ img_addnum.py
@@ -9,22 +9,24 @@ from PIL import Image,ImageFont,ImageDraw,ImageColor
 
 
 def img_addnum(img,num):
-    img1=Image.open("test1.jpg")
-    draw1= ImageDraw.Draw(img1)
-    font1= ImageFont.truetype("arial.ttf",size=60)
-    color1=ImageColor.getcolor("red","RGB")
+    img1=Image.open("test1.jpg") # 打开图片
+    draw1= ImageDraw.Draw(img1)  # 画
+    font1= ImageFont.truetype("arial.ttf",size=60) # 字体
+    color1=ImageColor.getcolor("red","RGB") # 颜色
     w,h=img1.size
-    draw1.text((w*0.9,h*0.05),num,fill=color1,font=font1)
+    draw1.text((w*0.9,h*0.05),num,fill=color1,font=font1) # 内容
     
-    img1.save("img_addnum.jpg","jpeg")
+    img1.save("img_addnum.jpg","jpeg") # 保存
     
     return 0
 
     
 
 
+if __name__ == '__main__':
+    img_addnum("test1.jpg","68")
+    
 
-img_addnum("test1.jpg","68")
     
 
     
